9635002569f70a16cccbbb299f71ac81c16bf94f
user <Global@mail.ua>
2019-05-08 19:05:22 +0300

    tasks

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<title>Laravel Quickstart - Basic</title>
+	<!-- CSS и JavaScript -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    </head>
+    <body>
+	<div class="container">
+	    <nav class="navbar navbar-default">
+		<!-- Содержимое Navbar -->
+	    </nav>
+	</div>
+	@yield('content')<!--вставляет содержимое @section('content')-->
+    </body>
+</html>
