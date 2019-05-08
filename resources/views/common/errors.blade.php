8dc1d3bc4b322039f5eede53dcc50931bda1527b
user <Global@mail.ua>
2019-05-08 19:41:50 +0300

    save tast and errors

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,1 +1,12 @@
-
+@if (count($errors) > 0)
+<!-- Список ошибок формы -->
+<div class="alert alert-danger">
+    <span style="margin: 5px">Упс! Что-то пошло не так!</span>
+    <br><br>
+    <ul>
+	@foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+	@endforeach
+    </ul>
+</div>
+@endif
